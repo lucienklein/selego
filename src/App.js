034475b62eac93879651c657542b2cfded75d105
Lucien Klein <lucien.kln@gmail.com>
2021-06-19 13:00:34 +0200

    ✨ feat: image search / display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    if (!search) {
+      setInfo([]);
+      return;
+    }
+
+    async function fetchData() {
+      const response = await fetch(
+        `https://pixabay.com/api/?key=17555297-46a99d3dc7abf78679ec9e640&q=${encodeURI(
+          search
+        )}&image_type=photo&pretty=true&per_page=6`
+      );
+      const { hits: data } = await response.json();
+      setInfo(data);
+    }
+    fetchData();
+  }, [search]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="search..."
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <div>
+        {info.map((el) => (
+          <img key={el.id} src={el.webformatURL} alt={el.tags} />
+        ))}
+      </div>
     </div>
   );
 }
